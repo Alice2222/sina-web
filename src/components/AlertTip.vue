f960eb03a4d5a1cd99c7f9a5c471ab3c2df9d814
Alice2222 <623387518@qq.com>
2017-07-21 18:24:02 +0800

    everyday update

---
+++ src/components/AlertTip.vue
@@ -0,0 +1,41 @@
+<template>
+	<div id="alert-tip">
+		<div class="page__demo-group">
+        <ui-confirm
+            ref="basicConfirm"
+            title="Basic Confirm"
+            @confirm="onConfirm"
+            @deny="onDeny">
+            {{alertText}}
+        </ui-confirm>
+    </div>
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return {
+				// confirmResult: ''
+        
+			}
+		},
+		props: ['alertText'],
+		methods:{
+      showConfirm(){
+        this.$refs.basicConfirm.open()
+      },
+      hideConfirm(ref) {
+          this.$refs[ref].close();
+      },
+			onConfirm() {
+          this.confirmResult = 'You confirmed the request.';
+      },
+      onDeny() {
+          this.confirmResult = 'You denied the request.';
+      }
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	
+</style>
--- src/views/singup/singup.vue
+++ src/views/singup/singup.vue
@@ -1,26 +1,48 @@
 <template>
 	<div class="login-box">
-		<img src="../../assets/top_line_bg.png" alt="">
+		<section class="top_line"></section>
 		<section class="logo">
 			<img src="../../assets/logo_big.png" alt="">
 		</section>
 		<main class="login-main">
-				<ui-tabs type="text">
-          <ui-tab title="个人注册">
-          	<!-- <Form :model="person" :label-width="80" :rules="rules">
-			        <Form-item label="手机号">
-			            <Input v-model="person.phone" placeholder="请输入手机号码" prop="phone"></Input>
-			        </Form-item>
-			      </Form> -->
-          </ui-tab>
-          <ui-tab title="官方注册">
-              Authors
-          </ui-tab>
-      	</ui-tabs>
+			<div class="tab-header">
+				<ul class="tab-list">
+					<div class="bottom-line"></div>	
+					<li class="tab-item" :class="{is_active: person_reg}" @click="handleClickTabItem(true)">
+						<section class="tab-item_text">
+							个人注册
+						</section>
+					</li>
+					<li class="tab-item"><span class="v-line"></span></li>
+					<li class="tab-item" :class="{is_active: !person_reg}"  @click="handleClickTabItem(false)">
+						<section class="tab-item_text">
+							官方注册
+						</section>
+					</li>
+					<li class="clearfix"></li>
+				</ul>
+			</div>
+			<div class="tab-content">
+				<transition name="fade">
+					<section class="person-reg" v-if="person_reg">
+						<div class="reg-form">
+							ccc
+						</div>
+					</section>
+					<section class="official-reg" v-if="!person_reg">
+						<div class="reg-form">
+							ddd
+						</div>
+					</section>
+				</transition>
+			</div>
+    	<ui-button @click="showConfirm('basic-confirm')">Basic Confirm</ui-button>
 		</main>
+		<alert-tip :alertText="alertText" ref="basic-confirm"></alert-tip>
 	</div>
 </template>
 <script>
+	import AlertTip from "@/components/AlertTip";
 	export default{
 		data(){
 			return{
@@ -29,20 +51,39 @@
 				},
 				rules: {
 					phone:[{required: true, message: '请填写手机号', trigger: 'blur' }]
-				}
+				},
+				alertText: '确认' ,
+				person_reg: false
 			}
 		},
 		created(){
-
+			this.handleClickTabItem(true)
 		},
-		component:{
-
+		components:{
+			AlertTip
 		},
 		computed:{
 
 		},
 		methods:{
-
+			showConfirm(ref) {
+          this.$refs[ref].showConfirm();
+      },
+      handleClickTabItem(person_reg){
+      	this.person_reg = person_reg;
+      	this.$nextTick(function(){
+      		let $tab_item = $(".tab-item"),
+      			$bottom_line = $(".bottom-line"),
+      			$active=  $(".is_active"),
+      			bottom_line_start = 10;
+      		$tab_item.each(function(index){
+	      		if(index < $active.index()-1){
+	      			bottom_line_start += $(this).outerWidth(true)
+	      		}
+	      	})
+	      	$bottom_line.animate({left:bottom_line_start, width: $active.outerWidth(false)},200)
+      	})
+      }
 		}
 	}
 </script>
@@ -52,11 +93,15 @@
 		background-repeat: repeat-x;
 		width: 100%;
 		min-height: 1000px;
+		.top_line{
+			background-image: url(../../assets/top_line_bg.png);
+			height: 3px;
+		}
 		img{
 			display: block
 		}
 		.logo{
-			margin: 100px auto 0px;
+			margin: 0 auto ;
 			width: 900px;
 			height: 186px;
 			background-image: url(../../assets/bg_reg_wave.png);
@@ -64,6 +109,7 @@
 			img{
 				margin: auto ;
 			}
+
 		}
 	}
 	.login-main{
@@ -73,5 +119,39 @@
 		box-shadow: 0px 2px 3px #98b4c4;
     -moz-box-shadow: 0px 2px 3px #98b4c4;
     -webkit-box-shadow: 0px 2px 3px #98b4c4;
+    background: #fff;
+    .tab-header{
+    	padding: 20px;
+			.tab-list{
+				position: relative;
+				.tab-item{
+					float: left;
+					font-size: 22px;
+					margin: 10px;
+					cursor: pointer;
+					.v-line{
+				    height: -32px;
+				    display: inline-block;
+				    height: 14px;
+				    width: 1px;
+				    background: #999;
+					}
+				}
+				.tab-item.is-active{
+					border-bottom: 2px ;
+				}
+				.bottom-line{
+					height: 3px;
+					width: 11px;
+					background: #ffa00a;
+					display: inline-block;
+					position: absolute;
+					bottom: 8px;
+				}
+				
+			}
+			
+    }
 	}
+
 </style>
